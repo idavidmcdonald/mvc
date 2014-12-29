44d158bd8b60f50a8f15b52b16abb3bd4e3dc859
David McDonald <idavidmcdonald@me.com>
2014-12-29 21:15:28 +0000

    bug fix on file name

--- library/bootstrap.php
+++ library/bootstrap.php
@@ -3,5 +3,5 @@
 require_once (ROOT . DS . 'config' . DS . 'config.php');
 require_once (ROOT . DS . 'config' . DS . 'routing.php');
 require_once (ROOT . DS . 'config' . DS . 'autoload.php');
-require_once (ROOT . DS . 'library' . DS . 'shared.php');
+require_once (ROOT . DS . 'library' . DS . 'application.php');
 $app = new Application();
